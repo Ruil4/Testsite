1f79f08c34f651cedfeed3edd55121867a3245df
Thomas <ruil4official@gmail.com>
2020-02-12 09:20:14 +0100

    Console Test Message Change

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
-console.log('Hallo du');
+
 let today = new Date();
 let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+console.log('Hallo du');
